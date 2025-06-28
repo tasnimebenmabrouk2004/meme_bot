e01f7b8ef9eaffc2e0da3d2d08ef077dbb193847
tasniiimeee <153118069+tasniiimeee@users.noreply.github.com>
2025-06-28 15:54:46 +0200

    🎉 Initial commit: Tasnime's meme bot

---
+++ bot.py
@@ -0,0 +1,34 @@
+import discord
+import requests
+import json
+
+def get_meme():
+    response = requests.get('https://meme-api.com/gimme')
+    json_data = json.loads(response.text)
+    return json_data['url']
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        # Avoid responding to itself
+        if message.author == self.user:
+            return
+
+        if message.content.startswith('$meme'):
+            await message.channel.send(get_meme())
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+
+
+
+client = MyClient(intents=intents)
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+client.run(os.getenv("DISCORD_TOKEN"))
+
